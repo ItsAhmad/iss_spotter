821ee2ae29ed0376e484a5d856ebae0fcd4dd75c
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-03-04 00:05:17 -0500

    implemented function to fetch the flyover times of ISS

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/*
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+*/
+
+
+
 
 /*
 const { fetchCoordsByIP } = require('./iss');
@@ -11,8 +28,6 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   console.log('It worked! Returned coordinates:' , coordinates);
 });
 */ 
-
-
 
 
 
