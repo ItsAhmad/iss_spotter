47495af07d1623886b1f1380a35a43da0c03e81d
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-03-03 23:51:21 -0500

    function to fetch ipv4 address setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+
+
+
+
+
+/* const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+}); */ 
