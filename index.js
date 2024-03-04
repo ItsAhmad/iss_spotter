0e5c9ddf65ffb9edff2160db16a2685495befc70
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-03-03 23:59:19 -0500

    implemented a function to fetch co-ordinates by IP

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+/*
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+*/ 
 
 
 
--- iss.js
+++ iss.js
@@ -1,3 +1,5 @@
+const request = require("request");
+
 /**
  * Makes a single API request to retrieve the user's IP address.
  * Input:
@@ -8,7 +10,7 @@
  */
 const fetchMyIP = function(callback) { 
   
-request('https://api.ipify.org?format=json', (error, resp, body) => {
+request('https://api.ipify.org?format=json', (error, response, body) => {
 
 if (error) {
   callback(error, null);
@@ -25,4 +27,43 @@ const ip = JSON.parse(body).ip;
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+
+const fetchCoordsByIP = function(ip, callback) {
+
+request(`http:ipwho.is/${ip}`, (error, response, body) => {
+
+  if (error) {
+    callback(error, null);
+    return;
+  }
+
+  const parsedBody = JSON.parse(body);
+
+  if (!parsedBody.success) {
+    const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+    callback(Error(message), null);
+    return;
+  } 
+
+  const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+  });
+};
+
+
+module.exports = {fetchCoordsByIP};
+
+
+
+
